849799f7684a6897b962db3e7e698fbc2cf9f259
Oliver Stenkilde <oliv6101@skivecollege.dk>
2020-01-23 13:03:13 +0100

    opretaktivitet 35% done

---
+++ PowerEvent/PowerEvent/Pages/OpretAktivitet.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace PowerEvent
+{
+    public class testModel : PageModel
+    {
+        [BindProperty]
+        public int[] SelectedTags { get; set; }
+        public SelectList TagOptions { get; set; }
+
+        public List<SelectListItem> People { get; set; }
+        public void OnGet()
+        {
+            People  = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "1", Text = "ivejrgiortgirtgTyr MaxSec" },
+                new SelectListItem { Value = "2", Text = "Pete" },
+                new SelectListItem { Value = "3", Text = "Katy" },
+                new SelectListItem { Value = "4", Text = "Carl" }
+            };
+        }
+        public void OnPost()
+        {
+
+        }
+        public void OnPostcmdSubmit()
+        {
+            
+        }
+    }
+}
