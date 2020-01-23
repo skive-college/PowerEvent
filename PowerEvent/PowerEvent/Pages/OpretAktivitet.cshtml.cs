db6ce5d57dd44e286b620e3173685ed85d0b837a
Oliver Stenkilde <oliv6101@skivecollege.dk>
2020-01-23 13:54:12 +0100

    opretAktivitet 36%

--- PowerEvent/PowerEvent/Pages/OpretAktivitet.cshtml.cs
+++ PowerEvent/PowerEvent/Pages/OpretAktivitet.cshtml.cs
@@ -10,6 +10,7 @@ namespace PowerEvent
 {
     public class testModel : PageModel
     {
+        public string Aktivitet { get; set; }
         [BindProperty]
         public int[] SelectedTags { get; set; }
         public SelectList TagOptions { get; set; }
@@ -19,7 +20,7 @@ namespace PowerEvent
         {
             People  = new List<SelectListItem>
             {
-                new SelectListItem { Value = "1", Text = "ivejrgiortgirtgTyr MaxSec" },
+                new SelectListItem { Value = "1", Text = "Tyr MaxSec" },
                 new SelectListItem { Value = "2", Text = "Pete" },
                 new SelectListItem { Value = "3", Text = "Katy" },
                 new SelectListItem { Value = "4", Text = "Carl" }
@@ -27,7 +28,9 @@ namespace PowerEvent
         }
         public void OnPost()
         {
-
+            string test;
+            Aktivitet = txtAktivitet;
+            test = Aktivitet;
         }
         public void OnPostcmdSubmit()
         {
