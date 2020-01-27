161e6a7a501bc3d007478ff1672ea9149ff36051
Oliver Stenkilde <oliv6101@skivecollege.dk>
2020-01-27 09:37:27 +0100

    blah

--- PowerEvent/PowerEvent/Pages/OpretAktivitet.cshtml.cs
+++ PowerEvent/PowerEvent/Pages/OpretAktivitet.cshtml.cs
@@ -6,10 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace PowerEvent
+namespace PowerEvent.Pages
 {
     public class testModel : PageModel
     {
+        [TempData]
         public string Aktivitet { get; set; }
         [BindProperty]
         public int[] SelectedTags { get; set; }
@@ -18,23 +19,26 @@ namespace PowerEvent
         public List<SelectListItem> People { get; set; }
         public void OnGet()
         {
-            People  = new List<SelectListItem>
+            if (People == null)
+            {
+                People = new List<SelectListItem>
             {
                 new SelectListItem { Value = "1", Text = "Tyr MaxSec" },
                 new SelectListItem { Value = "2", Text = "Pete" },
                 new SelectListItem { Value = "3", Text = "Katy" },
                 new SelectListItem { Value = "4", Text = "Carl" }
             };
+            }
         }
+        
         public void OnPost()
         {
-            string test;
-            Aktivitet = txtAktivitet;
-            test = Aktivitet;
-        }
-        public void OnPostcmdSubmit()
-        {
             
+        }
+        public void OnPostCmdSubmit()
+        {
+            string test;
+            test = Aktivitet;
         }
     }
 }
