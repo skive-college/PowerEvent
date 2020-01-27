10edc1bbc6a8e2ab32dd9b7c05e8a6762f675a3e
Jonas Skou Andersen <jona342g@skivecollege.dk>
2020-01-27 16:35:47 +0100

    save

--- PowerEvent/PowerEvent/Models/CustomPageBase.cs
+++ PowerEvent/PowerEvent/Models/CustomPageBase.cs
@@ -14,17 +14,20 @@ namespace PowerEvent.Models
         {
             tempData[key] = JsonConvert.SerializeObject(value);
         }
-        public static T Peek<T>(this ITempDataDictionary tempData, string key) where T : class
-        {
-            object o = tempData.Peek(key);
-            return o == null ? null : JsonConvert.DeserializeObject<T>((string)o);
-        }
 
         public static T Get<T>(this ITempDataDictionary tempData, string key) where T : class
         {
             tempData.TryGetValue(key, out object o);
             return o == null ? null : JsonConvert.DeserializeObject<T>((string)o);
         }
+
+        public static T Peek<T>(this ITempDataDictionary tempData, string key) where T : class
+        {
+            object o = tempData.Peek(key);
+            return o == null ? null : JsonConvert.DeserializeObject<T>((string)o);
+        }
+
+        
     }
 
 }
