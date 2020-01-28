9d84048f09a943fd8c4f69519e6f1065a06abd0d
Jonas Skou Andersen <jona342g@skivecollege.dk>
2020-01-28 12:22:26 +0100

    asdasdasadfas

--- PowerEvent/DatabaseClassLibrary/DBHandler.cs
+++ PowerEvent/DatabaseClassLibrary/DBHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace DatabaseClassLibrary
@@ -8,7 +9,29 @@ namespace DatabaseClassLibrary
     {
         private static readonly string connectionString = @"Data Source=planner.aspitweb.dk;Initial Catalog=PowerEvent;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
+        public static List<object> getUsers()
+        {
+            List<object> retur = new List<object>();
 
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string sql = "SELECT * FROM Event";
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    retur.Add(
+                        new { Id = int.Parse(reader["Id"].ToString()), Navn = reader["Navn"].ToString() }
+                        );
+                    reader.Close();
+                }
+
+                
+            }
+            return retur;
+        }
 
 
 
