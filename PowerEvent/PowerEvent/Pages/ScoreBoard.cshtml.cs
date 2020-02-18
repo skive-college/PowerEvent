03540f45cea57540af22b237b9fd27225bdc9135
Jumpbo <jumpbo7@gmail.com>
2020-02-18 09:12:05 +0100

    g

--- PowerEvent/PowerEvent/Pages/ScoreBoard.cshtml.cs
+++ PowerEvent/PowerEvent/Pages/ScoreBoard.cshtml.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PowerEvent.Helpers;
+using PowerEvent.Models;
 
 namespace PowerEvent
 {
     public class ScoreBoardModel : PageModel
     {
+        [BindProperty]
+        public int SelectedEvent { get; set; }
+
+        public List<Event> EventList { get; set; }
         public string EventName { get; set; }
         public string TeamEt { get; set; }
         public string TeamTo { get; set; }
@@ -17,6 +23,55 @@ namespace PowerEvent
             EventName = "test";
             TeamEt = "teamet";
             TeamTo = "teamto";
+            SelectedEvent = -1;
+            EventList = DBAdapter.getEvent();
+            checkListScript();
+        }
+
+        private void checkListScript()
+        {
+            try 
+            {
+                SelectedEvent = int.Parse(Request.Query["EventList"]);
+            }
+            catch
+            {
+            }
+
+            if (SelectedEvent == -1)
+            {
+                loadTempDataEvent();
+                if (SelectedEvent != -1)
+                {
+                    
+                }
+            }
+
+            string navn = Request.Query["navn"];
+            if (navn == "EventList")
+            {
+                if (SelectedEvent != -1)
+                {
+                    saveTempDataEvent();
+                    
+                }
+            }
+        }
+
+        private void saveTempDataEvent()
+        {
+            List<int> tempEventList = new List<int>();
+            tempEventList.Add(SelectedEvent);
+            TempData.Set("SelectedEventId", tempEventList);
+        }
+
+        private void loadTempDataEvent()
+        {
+            List<int> tempEventList = TempData.Peek<List<int>>("SelectedEventId");
+            if (tempEventList != null)
+            {
+                SelectedEvent = tempEventList[0];
+            }
         }
     }
 }
